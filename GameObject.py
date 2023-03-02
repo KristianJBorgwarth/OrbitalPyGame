1e7657839261ba03146e0e24912a21679f5341b5
Nichlas Hoberg <nichlashoberg@gmail.com>
2023-03-02 13:04:58 +0100

    Initial py commit

---
+++ GameObject.py
@@ -0,0 +1,17 @@
+﻿import pygame.image
+from abc import abstractmethod, ABC
+from Transform import Transform
+from overrides import override
+
+
+class GameObject(ABC):
+    def __init__(self, x, y, image, world):
+        self.transform = Transform(position=(x, y))
+        self.image = pygame.image.load(image)
+        self.world = world
+
+    def update(self):
+        pass
+
+    def draw(self, screen):
+        screen.blit(self.image, self.transform.position)
